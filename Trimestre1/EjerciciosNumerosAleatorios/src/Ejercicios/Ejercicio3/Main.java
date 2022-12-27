7169b0eb3c9bb4c3ee89c2344299959b317daeb1
Mateo <mateocarballo2@gmail.com>
2022-12-27 20:38:51 +0100

    Hecho con libreria importada comun a todos los ejercicios

--- Trimestre1/EjerciciosNumerosAleatorios/src/Ejercicios/Ejercicio3/Main.java
+++ Trimestre1/EjerciciosNumerosAleatorios/src/Ejercicios/Ejercicio3/Main.java
@@ -1,5 +1,6 @@
 package Ejercicios.Ejercicio3;
 
+import Utilidades.UtilidadesMatematicas;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,21 +8,13 @@ import java.io.InputStreamReader;
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
-       diaDeLaSemana();
+    public static void main(String[] args) {
+
+       String dias []={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+       System.out.println(dias[UtilidadesMatematicas.numAleatorio(0,6)]);
+
     }
 
-    public static void diaDeLaSemana()throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int entrada =0;
-
-        do {
-            System.out.println("Pulsa 1 para que te diga otro dia");
-            int aleatorio = (int) (Math.random() * (7) + 1);
-            System.out.println(aleatorio);
-            entrada=Integer.parseInt(br.readLine());
-        }while(entrada!=0);
-    }
 
 }
