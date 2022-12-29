768854d4c3a3d368bda680695647e77c40a02a1e
Mateo <mateocarballo2@gmail.com>
2022-12-29 12:06:31 +0100

    hasta ej 5

---
+++ Trimestre1/EjerciciosNumerosAleatorios/src/Ejercicios/Ejercicio4/Main.java
@@ -0,0 +1,33 @@
+package Ejercicios.Ejercicio4;
+
+import Utilidades.UtilidadesMatematicas;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException{
+        int continuar=1;
+        int dado;
+        int suma;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        do{
+            dado=0;
+            suma=0;
+            for (int i = 1; i <= 3; i++) {
+              dado=UtilidadesMatematicas.numAleatorio(1,6);
+              System.out.print("Tirada "+i+"->");
+              System.out.print(dado+" ");
+              suma+=dado;
+            }
+            System.out.println(" ");
+            System.out.println("La tirada suma -> "+suma);
+            System.out.println("Otra tirada? (1=Si/0=No)");
+            continuar=Integer.parseInt(br.readLine());
+        }while(continuar!=0);
+
+    }
+}
---
+++ Trimestre1/EjerciciosNumerosAleatorios/src/Ejercicios/Ejercicio5/Main.java
@@ -0,0 +1,22 @@
+package Ejercicios.Ejercicio5;
+
+import Utilidades.UtilidadesMatematicas;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        // Tomamos como reintegro la ultima posición
+        int combinacion[]=new int [6];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < combinacion.length; i++) {
+            combinacion[i]=UtilidadesMatematicas.numAleatorio(1,49);
+            System.out.print(combinacion[i]+" ");
+        }
+
+
+    }
+}
