4c829e452a08e7a1d1a15f92bd0fd89d5451b933
Mateo <mateocarballo2@gmail.com>
2023-01-09 23:30:14 +0100

    Ejercicio6 completo

---
+++ Trimestre2/EjerciciosArrays/src/Ejercicio6/Main.java
@@ -0,0 +1,26 @@
+package Ejercicio6;
+
+import misUtilidades.utilidadesMatematicas;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int longitudVector=0;
+        int valorPosiciones=0;
+        System.out.println("Longitud del vector?(la primera posicion es la cero)");
+        longitudVector=Integer.parseInt(br.readLine());
+        System.out.println("Valor para rellenar?");
+        valorPosiciones=Integer.parseInt(br.readLine());
+        int[] matrizEnteros =new int[longitudVector];
+
+        for (int i = 0; i < matrizEnteros.length; i++) {
+            matrizEnteros[i]=valorPosiciones;
+            System.out.println("Posicion-> "+i+" -> "+matrizEnteros[i]);
+        }
+
+    }
+
+}
