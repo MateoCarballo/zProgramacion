dde0c56143f68c108d4be1f1e2403538eca980af
Mateo <mateocarballo2@gmail.com>
2023-01-09 11:24:17 +0100

    Comienzo ejercicio2

---
+++ Trimestre2/EjerciciosArrays/src/Ejercicio1/Main.java
@@ -0,0 +1,20 @@
+package Ejercicio1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        float matrizReales[]=new float[10];
+        for (int i = 0; i < matrizReales.length; i++) {
+            System.out.println("Introduce el numero de la posicion "+i);
+            matrizReales[i]=Float.parseFloat(br.readLine());
+        }
+        for (int i = 0; i < matrizReales.length; i++) {
+            System.out.println("Posicion "+ i+"->"+matrizReales[i]);
+        }
+    }
+}
