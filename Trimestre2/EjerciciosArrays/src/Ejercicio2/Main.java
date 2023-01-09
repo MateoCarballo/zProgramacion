f5ce2c63c999f16282dbf40316c271751ab9185d
Mateo <mateocarballo2@gmail.com>
2023-01-09 11:29:30 +0100

    Ejercicio2 acabado

--- Trimestre2/EjerciciosArrays/src/Ejercicio2/Main.java
+++ Trimestre2/EjerciciosArrays/src/Ejercicio2/Main.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        double acumulado=0;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         float matrizReales[]=new float[10];
@@ -14,7 +15,9 @@ public class Main {
             matrizReales[i]=Float.parseFloat(br.readLine());
         }
         for (int i = 0; i < matrizReales.length; i++) {
-            System.out.println("Posicion "+ i+"->"+matrizReales[i]);
+            acumulado=acumulado+matrizReales[i];
         }
+        System.out.println("El resultado de la suma de todos los valores introducidos es->"+acumulado);
+
     }
 }
