76e57857fb70ab80f2e8e362327472a5acd94898
Mateo <mateocarballo2@gmail.com>
2023-01-09 23:37:13 +0100

    Ejercicio7 extrictamente menor que limite superior

---
+++ Trimestre2/EjerciciosArrays/src/Ejercicio7/Main.java
@@ -0,0 +1,29 @@
+package Ejercicio7;
+
+import misUtilidades.utilidadesMatematicas;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int inicio=0;
+        int fin=0;
+        int diferencia=0;
+        System.out.println("Que numero ocupa la primera posicion de la matriz?");
+        inicio=Integer.parseInt(br.readLine());
+        System.out.println("Que numero ocupa la ultima posicion?");
+        fin=Integer.parseInt(br.readLine());
+        diferencia=fin-inicio;
+        int[] matrizEnteros =new int[diferencia];
+
+        for (int i = 0; i < matrizEnteros.length; i++) {
+            matrizEnteros[i]=inicio;
+            System.out.println("Posicion-> "+i+" -> "+matrizEnteros[i]);
+            inicio++;
+        }
+
+    }
+
+}
