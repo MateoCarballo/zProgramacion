9c1f4641891512870629011700dff15a4502d5ae
Mateo <mateocarballo2@gmail.com>
2023-01-09 23:24:00 +0100

    Ejercicio 5 completo

---
+++ Trimestre2/EjerciciosArrays/src/Ejercicio5/Main.java
@@ -0,0 +1,33 @@
+package Ejercicio5;
+
+import misUtilidades.utilidadesMatematicas;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[] matrizEnteros =new int[20];
+
+        llenarMatrizEnteros(matrizEnteros);
+        System.out.println("La media de todos los numeros introducidos es -> "+ mediaAcumuladoMatriz(matrizEnteros));
+
+    }
+
+    public static void  llenarMatrizEnteros(int matriz[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("Introduce el numero de la posicion "+i);
+            matriz[i]=Integer.parseInt(br.readLine());
+        }
+    }
+
+    public static int mediaAcumuladoMatriz(int [] matriz){
+        int acumulado=0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            acumulado= acumulado+matriz[i];
+        }
+        return (acumulado/matriz.length);
+    }
+}
