c528ad1916e271f73b6fcf7225d9a0194284bc0b
Mateo <mateocarballo2@gmail.com>
2023-01-10 10:22:17 +0100

    Editando commit previo

---
+++ Trimestre2/EjerciciosArrays/src/Ejercicio11/Main.java
@@ -0,0 +1,42 @@
+package Ejercicio11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int tamanoMatriz = 10;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] matrizInicio = new int[tamanoMatriz];
+        int[] matrizFin = new int[tamanoMatriz];
+        llenarMatriz(matrizInicio);
+
+        inversaMatriz(matrizInicio,matrizFin);
+        System.out.println("Matriz original");
+        escribirMatriz(matrizInicio);
+        System.out.println("\n");
+        System.out.println("Matriz inversa");
+        escribirMatriz(matrizFin);
+        System.out.println("\n");
+    }
+    public static void llenarMatriz(int[] miMatriz){
+        for (int i = 0; i < miMatriz.length; i++) {
+        miMatriz[i]=i;
+        }
+    }
+    public static void inversaMatriz(int[] originalMatriz,int[]inversaMatriz){
+        int longitudMatriz = (originalMatriz.length-1);
+        for (int i = 0; i < originalMatriz.length; i++) {
+                inversaMatriz[longitudMatriz]=originalMatriz[i];
+                longitudMatriz--;
+        }
+    }
+    public static void escribirMatriz(int[]miMatriz){
+        for (int i = 0; i < miMatriz.length ; i++) {
+            System.out.println("Posicion "+i+"-->"+miMatriz[i]);
+        }
+
+    }
+}
